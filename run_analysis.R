b705bd4a514ff2eb055f3ae70b7834bf5b0b41c7
vikrambiswas <vikramstl@gmail.com>
2017-07-17 21:16:24 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,10 @@
+#Code should be able to do the following 5 things: 
+# 1) Merges the training and the test sets to create one data set.
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3) Uses descriptive activity names to name the activities in the data set
+# 4) Appropriately labels the data set with descriptive variable names.
+# 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 #Loading Packages and Committing Data
 > packages <- c("data.table", "reshape2")
 > sapply(packages, require, character.only=TRUE, quietly=TRUE)
